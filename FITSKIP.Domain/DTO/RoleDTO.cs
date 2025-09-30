bf5d4528d7edf59727f14a8e0568526c67980778
Trungndhe172845 <Trungndhe172845@fpt.edu.vn>
2025-10-01 02:25:56 +0700

    CRUD API Import lít User

---
+++ FITSKIP.Domain/DTO/RoleDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FITSKIP.Domain.DTO
+{
+    public class RoleDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string NormalizedName { get; set; } = null!;
+        public string ConcurrencyStamp { get; set; }
+    }
+}
---
+++ FITSKIP.Domain/Interfaces/IRoleRepository.cs
@@ -0,0 +1,19 @@
+﻿using FITSKIP.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FITSKIP.Domain.Interfaces
+{
+    public interface IRoleRepository
+    {
+        Task<IReadOnlyList<IdentityRole>> GetRolesAsync(CancellationToken cancellationToken = default);
+        Task<IdentityRole> CreateRoleAsync(IdentityRole role, CancellationToken cancellationToken = default);
+        Task<IdentityRole?> UpdateRoleAsync(IdentityRole role, CancellationToken cancellationToken = default);
+        Task<IdentityRole?> GetRoleByIdAsync(string id, CancellationToken cancellationToken = default);
+        Task<IdentityRole?> DeleteRoleAsync(string id, CancellationToken cancellationToken = default);
+    }
+}
