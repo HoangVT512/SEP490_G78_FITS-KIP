59d97fa17885b2c90f15aa8e39531d190189dffe
annthe176161 <annthe176161@fpt.edu.vn>
2025-10-16 17:09:44 +0700

    Add Issue field to Equipment entity and create migration to update database

--- FITSKIP.Domain/Entities/Equipment.cs
+++ FITSKIP.Domain/Entities/Equipment.cs
@@ -19,6 +19,8 @@ public partial class Equipment
 
     public string? Qrcode { get; set; }
 
+    public string? Issue { get; set; }
+
     public int? StageId { get; set; }
 
     public bool IsActive { get; set; } = true;
---
+++ FITSKIP.Infrastructure/Migrations/20251016100558_AddIssueToEquipment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FITSKIP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIssueToEquipment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Issue",
+                table: "Equipment",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Issue",
+                table: "Equipment");
+        }
+    }
+}
